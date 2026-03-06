5b6b69d2bb5082f3b51697648be5dfd2603464b8
emanuele-midori <emanuele@midorisrl.eu>
2026-03-06 18:05:13 +0100

    Calcolatirce funzionante

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
 screenMaxLength = 15;
 
-let number = '';
-let numberTemp = '';
-let operator = ''
+let num1 = '';
+let num2 = '';
 let result = '';
-let previousResult = ''
-let screenNumber = ''
+let operator = '';
 
+let isFirstDigit = true
+let isSecondDigit = false
+let isOperator = false
+let enableOperators = false
 
 const screen = document.querySelector('.screen-number')
 screen.textContent = '0'
@@ -15,31 +17,40 @@ const operators = document.querySelector('.operators')
 
 function prepareDigits() {
     digits.addEventListener('click', (e) => {
-        //console.log(e.target.tagName);
-        //console.log(e.target.className);
         if (e.target.tagName === 'BUTTON') {
-            switch (e.target.className) {
-                case 'zero':
-                    // Se premo 0 all'inizio non succede niente
-                    if (number !== '' && number !== '0' && number.length !== 0) {
-                        if(number.length < screenMaxLength){
-                            number = number.concat(e.target.textContent);
-                            screenNumber = number
-                            updateScreen(screenNumber);
-                        }
-                    }
-                    break;
-                default:
-                    if(number.length < screenMaxLength){
 
-                        if(number === '0'){
-                            number = '';
-                        }
+            enableOperators = true
 
-                        number = number.concat(e.target.textContent);
-                        screenNumber = number
-                        updateScreen(screenNumber);
+            isOperator = false;
+            if(num1 !== '' && !isFirstDigit){
+                isSecondDigit = true;
+            }
+            if(num2 !== '' && !isSecondDigit){
+                isFirstDigit = true;
+            }
+
+            if(isFirstDigit) {
+                if(e.target.className ==='zero' && (num1 === '' || num1 === '0')){
+                }  else {
+                    isSecondDigit = false;
+                    isOperator = false;
+                    console.log(e.target.textContent);
+                    if(num1.length < screenMaxLength){
+                        num1 = num1.concat(e.target.textContent);
                     }
+                    updateScreen(num1)
+                }
+            } else if(isSecondDigit) {
+                if(e.target.className ==='zero' && (num2 === '' || num2 === '0')){
+                }  else {
+                    isFirstDigit = false;
+                    isOperator = false;
+                    console.log(e.target.textContent);
+                    if(num2.length < screenMaxLength){
+                        num2 = num2.concat(e.target.textContent);
+                    }
+                    updateScreen(num2)
+                }
             }
         }
     });
@@ -50,66 +61,102 @@ function prepareOperators() {
     operators.addEventListener('click', (e) => {
         if (e.target.tagName === 'BUTTON') {
 
-            switch (e.target.className){
-                case 'equal':
-                    doTheMath()
-                    break;
-                case 'clear':
-                    clearScreen()
-                    break;
-                default:
-                    if(previousResult.length>0){
-                        // SE SONO GIA' ALLA SECONDA OPERAZIONE
-                        doTheMath()
-                    } else{
-                        numberTemp = number
-                        number = ''
-                        operator = e.target.className;
-                        updateScreen(e.target.textContent);
-                    }
-                    break;
+            if(enableOperators){
+
+                isOperator = true;
+                isFirstDigit = false;
+                isSecondDigit = false;
+
+                switch (e.target.className){
+                    case 'equal':
+                        if(num1 !== '' && num2 !== ''){
+                            doTheMath()
+                        }
+                        break;
+                    case 'clear':
+                        clearScreen()
+                        break;
+                    default:
+                        if(num1 !== '' && num2 !== ''){
+                            doTheMath()
+                        } else{
+                            operator = e.target.className;
+                            updateScreen(e.target.textContent);
+                        }
+                        break;
+                }
             }
         }
     })
 }
 
 function clearScreen() {
-    numberTemp = ''
-    number = ''
+    num1 = ''
+    num2 = ''
     result = ''
-    previousResult = ''
-    screenNumber = '0'
     operator = ''
-    updateScreen(screenNumber);
+    isFirstDigit = true;
+    isSecondDigit = false;
+    isOperator = false;
+    enableOperators = false;
+    updateScreen('0')
 }
 
 function doTheMath() {
-    if(operator.length > 0){
-        if(number.length > 0 && numberTemp.length > 0){
-            switch (operator){
-                case 'plus':
-                    previousResult = (parseInt(number) + parseInt(numberTemp)).toString();
-                    number = ''
-                    numberTemp = ''
-                    result = ''
-                    updateScreen(previousResult);
-            }
-        }
+
+    switch (operator) {
+        case 'plus':
+            result = (parseInt(num1) + parseInt(num2)).toString().substring(0,15);
+            updateScreen(result);
+            num1 = result;
+            num2 = '';
+            isFirstDigit = false;
+            isSecondDigit = true;
+            isOperator = false;
+            break;
+        case 'minus':
+            result = (parseInt(num1) - parseInt(num2)).toString().substring(0,15);
+            updateScreen(result);
+            num1 = result;
+            num2 = '';
+            isFirstDigit = false;
+            isSecondDigit = true;
+            isOperator = false;
+            break;
+        case 'times':
+            result = (parseInt(num1) * parseInt(num2)).toString().substring(0,15);
+            updateScreen(result);
+            num1 = result;
+            num2 = '';
+            isFirstDigit = false;
+            isSecondDigit = true;
+            isOperator = false;
+            break;
+        case 'divide':
+            result = (parseInt(num1) / parseInt(num2)).toString().substring(0,15);
+            updateScreen(result);
+            num1 = result;
+            num2 = '';
+            isFirstDigit = false;
+            isSecondDigit = true;
+            isOperator = false;
+            break;
     }
 }
 
 function updateScreen(value) {
     screen.textContent = value;
-    console.log('- - -')
-    console.log('Number',number)
-    console.log('NumberTemp',numberTemp)
-    console.log('Operator',operator)
-    console.log('Result',result)
+
+    console.log('num1',num1);
+    console.log('num2',num2);
+    console.log('operator',operator);
+    console.log('result',result);
+
 }
 
 function main() {
-    prepareDigits();
-    prepareOperators();
+    prepareDigits()
+    prepareOperators()
 }
 
 main();
